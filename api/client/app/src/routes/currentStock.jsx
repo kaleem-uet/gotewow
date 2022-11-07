884de797ad2cca1d002167e4d337103b350b595e
kaleem-uet <kaleemullahk989@gmail.com>
2022-11-07 18:18:17 +0500

    app move to api

---
+++ api/client/app/src/routes/currentStock.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Statusbar from '../components/Statusbar';
+
+export default function CurrentStock() {
+    const [stocks, setStocks] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://glacial-beach-87404.herokuapp.com/api/currentStock`)
+            .then(res => { setStocks(res.data) })
+    }, [])
+
+    return (
+        <>
+        <Statusbar />
+        <main class="max-h-full">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="max-w-2xl mx-auto py-4 sm:py-8 lg:py-16 lg:max-w-none">
+                    <h2 class="text-2xl font-extrabold text-gray-900 text-center align-top ">
+                        Current Stock
+                    </h2>
+                    <div class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
+                        {
+                            stocks.length > 0 ?
+                                stocks.map((stock) => (
+
+                                    <div class="group relative mb-10">
+                                        <div class="relative w-full bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+                                            <img
+                                                src={stock.image}
+                                                alt="..."
+                                                class="w-full h-full object-center object-cover"
+                                            ></img>
+                                        </div>
+                                        <h3 class="mt-5 text-center text-base font-semibold text-gray-900">
+                                            {stock.name}
+                                        </h3>
+                                        <span class="font-bold text-indigo-900 absolute inset-0 p-2">
+                                            {`${stock.views} people are viewing`}
+                                        </span>
+                                        <h3 class="text-center text-base text-gray-500">
+                                            <a href={`/carousel?category=${stock.name}`}>
+                                                <span class="absolute inset-0"></span>
+                                                {`${stock.items_left} items in stock`}
+                                            </a>
+                                        </h3>
+                                    </div>
+
+                                ))
+                                :
+                                <div>There are no items in the current stock</div>
+                        }
+                    </div>
+                </div>
+            </div>
+        </main>
+        </>
+    );
+}
---
+++ api/client/app/src/routes/howItWorks.jsx
@@ -0,0 +1,30 @@
+
+import Statusbar from "../components/Statusbar"
+export default function HowItWorks() {
+    return (
+        <>
+        <Statusbar />
+        <main class="flex items-center flex-col justify-center p-10">
+            <div class="flex items-center justify-center">
+                <iframe
+                    width="700"
+                    height="375"
+                    src="https://www.youtube.com/embed/MSevAi_YarQ"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                >
+                </iframe>
+            </div>
+            <div class="p-10 w-5/6 leading-loose">
+                EDTEX FZC (Free Zone Company) is based in Sharjah, UAE in the special economic Free Zone. With in depth knowledge of the vintage/used branded wholesale and retail markets worldwide we offer specialized solutions to meet clients needs.
+            </div>
+            <div class="px-10 w-5/6 leading-loose">
+                <h2>Our Core Offerings are:</h2>
+                Wholesale - Due to our strategic location, favourable tax benefits and great shipping routes by air and sear, we are able to sort our own rag and deliver a fine grade of used branded clothing to clients at scale with consistency.
+            </div>
+        </main>
+        </>
+    );
+}
